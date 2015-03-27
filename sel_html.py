eb0e93f7562003da46372f82d1ef0a38c27a9d5d
sduggan <shane@zefr.com>
2015-03-26 21:01:21 -0700

    Selenium scraper file

--- sel_html.py
+++ sel_html.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
 
-driver = webdriver.Firefox()
-driver.get('http://www.yelp.com/search?find_desc=&find_loc=Venice%2C+CA&ns=1&ls=71baa56a7602cc86')
-print driver.html
+driver = webdriver.Firefox() 
+driver.get ('http://www.yelp.com/search?find_desc=&find_loc=Venice%2C+CA&ns=1&l\ s=71baa56a7602cc86') 
+print str(driver.page_source) 
 driver.quit()
+ 
+with open ('myfile', 'a') as f: f.write ('hi there\n')
+ 
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(open("index.html"))
+soup = BeautifulSoup ("<html>data</html>") 
+
+
+
+
